5517c7cb430aac4b8cebdfe252bead11fb2100ff
Kid Max <kidmax285@gmail.com>
2024-10-11 21:53:39 +0600

    update donation page

--- src/sections/view/donate-view.tsx
+++ src/sections/view/donate-view.tsx
@@ -1,17 +1,16 @@
 'use client';
 
 import Image from 'next/image';
-import sideLogo from '@/public/assets/logo/deepayan-logo.png';
+import sideLogo from '@/public/assets/images/donation.webp';
 import bKashLogo from '@/public/assets/logo/donate/bkash.svg';
 import nagadLogo from '@/public/assets/logo/donate/nagad.svg';
-import bgImage from '@/public/assets/uploads/home-banner-04.jpg';
 import sonaliBankLogo from '@/public/assets/logo/donate/sonali-bank.svg';
 
 import { Box, Grid, Container, Typography } from '@mui/material';
 
 import { useTranslate } from 'src/locales';
-import { varAlpha } from 'src/theme/styles';
 import { banglaFont } from 'src/theme/core';
+import { varAlpha, bgGradient } from 'src/theme/styles';
 
 const methods = [
   {
@@ -47,33 +46,20 @@ export function DonateView() {
         py: { xs: 14, sm: 20, lg: 24 },
       }}
     >
-      <Image
-        src={bgImage}
-        priority
-        height={1000}
-        width={500}
-        placeholder="blur"
-        alt="Photo by Fakurian Design"
-        style={{
-          position: 'absolute',
-          inset: 0,
-          height: '100%',
-          width: '100%',
-          objectFit: 'cover',
-          objectPosition: 'center',
-        }}
-      />
-
       <Box
         sx={{
           position: 'absolute',
-          inset: 0,
-          background:
-            'linear-gradient(to top right, rgba(0, 121, 184, 0.1), rgba(128, 0, 128, 0.3), rgba(255, 20, 147, 0.9))',
+          top: 0,
+          left: 0,
+          height: '100%',
+          width: '100%',
           opacity: 0.55,
+          ...bgGradient({
+            imgUrl: '/assets/uploads/home-banner-04.jpg',
+            color: `to top right, rgba(0, 121, 184, 0.1), rgba(128, 0, 128, 0.3), rgba(255, 20, 147, 0.5)`,
+          }),
         }}
       />
-      <Box sx={{ position: 'absolute', inset: 0, backgroundColor: 'rgba(18, 24, 32, 0.5)' }} />
 
       <Container maxWidth="lg">
         <Grid container spacing={8} sx={{ position: 'relative' }}>
@@ -88,7 +74,6 @@ export function DonateView() {
               component="h2"
               sx={{ fontWeight: 'bold', color: 'white', mb: 3, fontFamily: banglaFont }}
             >
-              {/* Your Donation Can <br /> Change the World */}
               {t('changeTheWorld')}
             </Typography>
             <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
@@ -112,10 +97,9 @@ export function DonateView() {
                     <Image
                       src={logo}
                       alt={name}
-                      layout="responsive"
                       width={144} // Use relative dimensions (multiplied 24px by 6)
                       height={144} // Use the same ratio as width (auto behavior)
-                      style={{ objectFit: 'contain' }} // Ensure it scales properly
+                      style={{ objectFit: 'contain', height: 'auto' }} // Ensure it scales properly
                     />
                   </Box>
                   <Box>
@@ -136,13 +120,14 @@ export function DonateView() {
               ))}
             </Box>
           </Grid>
+
           <Grid item xs={12} md={7} sx={{ display: 'flex', alignItems: 'center' }}>
             <Box sx={(theme) => ({ boxShadow: theme.customShadows.error, borderRadius: '1rem' })}>
               <Image
                 quality={100}
                 src={sideLogo}
                 alt="Deepayan Logo"
-                style={{ borderRadius: '1rem', width: '100%' }} // Adjust width as needed
+                style={{ borderRadius: '1rem', width: '100%', height: 'auto' }}
               />
             </Box>
           </Grid>
